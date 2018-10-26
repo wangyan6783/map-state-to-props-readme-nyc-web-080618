1616e99a20f739f4483f90e8029ae11a5c049599
echeng <wangyan6783@gmail.com>
2018-10-26 15:20:29 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -3,22 +3,6 @@ import './App.css';
 
 class App extends Component {
 
-  handleOnClick() {
-    this.props.store.dispatch({
-      type: 'INCREASE_COUNT',
-    });
-  }
 
-  render() {
-    return (
-      <div className="App">
-        <button onClick={(event) => this.handleOnClick()}>
-          Click
-        </button>
-        <p>{this.props.items.length}</p>
-      </div>
-    );
-  }
-};
 
 export default App;
